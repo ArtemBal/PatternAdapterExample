fd624942a4b0f7edb09e6e9d9a9ab35dcd0afe50
Artem Balan <a.s.balan96@gmail.com>
2023-06-17 22:19:46 +0300

    Simple example of an Adapter pattern

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import example.adapters.SquarePegAdapter;
+import example.round.RoundHole;
+import example.round.RoundPeg;
+import example.square.SquarePeg;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RoundHole hole = new RoundHole(5);
+        RoundPeg rpeg = new RoundPeg(5);
+        if (hole.fits(rpeg)) {
+            System.out.println("Round peg r5 fits round hole r5.");
+        }
+
+        SquarePeg smallSqPeg = new SquarePeg(2);
+        SquarePeg largeSqPeg = new SquarePeg(20);
+        // hole.fits(smallSqPeg); // Не скомпилируется.
+
+        // Адаптер решит проблему.
+        SquarePegAdapter smallSqPegAdapter = new SquarePegAdapter(smallSqPeg);
+        SquarePegAdapter largeSqPegAdapter = new SquarePegAdapter(largeSqPeg);
+        if (hole.fits(smallSqPegAdapter)) {
+            System.out.println("Square peg w2 fits round hole r5.");
+        }
+        if (!hole.fits(largeSqPegAdapter)) {
+            System.out.println("Square peg w20 does not fit into round hole r5.");
+        }
+    }
+}
---
+++ src/example/adapters/SquarePegAdapter.java
@@ -0,0 +1,21 @@
+package example.adapters;
+
+import example.round.RoundPeg;
+import example.square.SquarePeg;
+
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        // Рассчитываем минимальный радиус, в который пролезет этот колышек.
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+}
+
